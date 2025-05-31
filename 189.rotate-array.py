5ea8ca66d0c90e442183f0948d4691944b34839e
Sunwoo Baek <62567659+sbaek21@users.noreply.github.com>
2025-05-31 15:04:16 +0900

    189. Rotate Array

--- 189.rotate-array.py
+++ 189.rotate-array.py
@@ -12,10 +12,15 @@ class Solution(object):
         :type k: int
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        k = k % len(nums)
+        # Get the actual number of rotations
+        k = k % len(nums)      
+        # Get the number of elements to move from the end to the beginning
         r = len(nums) - k
+        # Store the elements to move
         new = nums[0:r]
+        # Remove the elements from the beginning
         nums[0:r] = []
+        # Append the stored elements to the end
         nums.extend(new)
 # @lc code=end
 
